257778ccee98cb287ff02137576fe2d0f814b17b
Sheldon Bean <sheldon.bean@gmail.com>
2021-06-03 22:55:39 -0600

    StatsSO

---
+++ Tactical RPG/Assets/Scripts/StatsSO.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class StatsSO : ScriptableObject
+{
+    public int lvl,
+        hp,
+        mp,
+        exp,
+        attack,
+        defense,
+        agility,
+        move;
+
+
+}
