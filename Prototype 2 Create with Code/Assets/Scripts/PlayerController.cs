4ec02151041e91cbdb03b88d30d88d0c0894167a
Sheldon Bean <sheldon.bean@gmail.com>
2021-07-05 17:30:53 -0600

    Create with Code 2.1

---
+++ Prototype 2 Create with Code/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 leftBoundary;
+    private Vector3 rightBoundary;
+    
+    private float horizontalInput;
+
+    public float speed = 10f;
+    public float xRange = 10f;
+
+    void Start()
+    {
+        leftBoundary = new Vector3(-xRange, transform.position.y, transform.position.z);
+        rightBoundary = new Vector3(xRange, transform.position.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * (horizontalInput * Time.deltaTime * speed));
+       
+        if (transform.position.x < -xRange)
+        {
+            transform.position = leftBoundary;
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = rightBoundary;
+        }
+        
+    }
+}
