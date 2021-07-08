0d3c92e917f0eeac382b2aa13133854ede29e828
Sheldon Bean <sheldon.bean@gmail.com>
2021-07-07 22:06:39 -0600

    createwithcode 2.4

--- Prototype 2 Create with Code/Assets/Scripts/SpawnManager.cs
+++ Prototype 2 Create with Code/Assets/Scripts/SpawnManager.cs
@@ -13,15 +13,19 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 20f;
     private float spawnPosZ = 20f;
 
+    public float startDelay = 2f;
+    public float spawnInterval = 1.5f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
     
 
-    void Update()
+    private void SpawnRandomAnimal()
     {
-        if (enabled)
-        {
-            animalSpawn = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], animalSpawn, animalPrefabs[animalIndex].transform.rotation);
-        } 
+        animalSpawn = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], animalSpawn, animalPrefabs[animalIndex].transform.rotation);
     }
 }
