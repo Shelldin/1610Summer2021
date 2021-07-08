28ef54f6f0adc99cf8a4d63c17bfaad1989a2f5e
Sheldon Bean <sheldon.bean@gmail.com>
2021-07-07 21:52:10 -0600

    create with code 2.3

---
+++ Prototype 2 Create with Code/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    
+    
+    private Vector3 animalSpawn;
+    private float spawnRangeX = 20f;
+    private float spawnPosZ = 20f;
+
+    
+
+    void Update()
+    {
+        if (enabled)
+        {
+            animalSpawn = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], animalSpawn, animalPrefabs[animalIndex].transform.rotation);
+        } 
+    }
+}
