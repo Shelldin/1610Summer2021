cf7fdaf67d84ee8830efea92342e129543794254
Sheldon Bean <sheldon.bean@gmail.com>
2021-07-09 14:08:24 -0600

    continuing battle menu tutorial

--- Tactical RPG/Assets/Scripts/MenuButtonController.cs
+++ Tactical RPG/Assets/Scripts/MenuButtonController.cs
@@ -16,10 +16,17 @@ public class MenuButtonController : MonoBehaviour
 
     private int verticalMovement;
 
+    public float yOffset = 64;
+
+    private Vector2 rectTransOffsetVector2,
+        alternateRectTransOffsetVector2;
+
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         isPressedUp = isPressedDown = false;
+        rectTransOffsetVector2 = new Vector2(0, yOffset);
+        alternateRectTransOffsetVector2 = new Vector2(0, (maxMenuIndex-2) * yOffset);
     }
 
     private void Update()
@@ -43,6 +50,38 @@ public class MenuButtonController : MonoBehaviour
         {
             if (!keyDown)
             {
+                if (verticalMovement < 0)
+                {
+                    if (menuIndex < maxMenuIndex)
+                    {
+                        menuIndex++;
+                        if (menuIndex > 1 && menuIndex < maxMenuIndex)
+                        {
+                            rectTransform.offsetMax += rectTransOffsetVector2;
+                        }
+                    }
+                    else
+                    {
+                        menuIndex = 0;
+                        rectTransform.offsetMax = Vector2.zero;
+                    }
+                }
+                else if (verticalMovement > 0)
+                {
+                    if (menuIndex > 0)
+                    {
+                        menuIndex--;
+                        if (menuIndex < maxMenuIndex - 1 && menuIndex > 0)
+                        {
+                            rectTransform.offsetMax -= rectTransOffsetVector2;
+                        }
+                    }
+                    else
+                    {
+                        menuIndex = maxMenuIndex;
+                        rectTransform.offsetMax = alternateRectTransOffsetVector2;
+                    }
+                }
                 keyDown = true;
             }
         }
