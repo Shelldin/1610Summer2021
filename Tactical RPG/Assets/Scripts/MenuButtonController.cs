15d97f473e55de37763f11eebcf287bdb67b5028
Sheldon Bean <sheldon.bean@gmail.com>
2021-08-05 02:38:06 -0600

    more menu stuff

--- Tactical RPG/Assets/Scripts/MenuButtonController.cs
+++ Tactical RPG/Assets/Scripts/MenuButtonController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(RectTransform))]
 //from PavCreations Tutorial https://pavcreations.com/scrollable-menu-in-unity-with-button-or-key-controller/#script-controlling-button-behaviour
-//START FROM "CHANGING THE POSITION ON THE LIST WITH KEYS" in tutorial
 public class MenuButtonController : MonoBehaviour
 {
     public int menuIndex,
@@ -19,15 +18,15 @@ public class MenuButtonController : MonoBehaviour
 
     public float yOffset = 64;
 
-    private Vector2 rectTransOffsetVector2,
-        alternateRectTransOffsetVector2;
+    //private Vector2 rectTransOffsetVector2,
+        //alternateRectTransOffsetVector2;
 
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
         isPressedUp = isPressedDown = false;
-        rectTransOffsetVector2 = new Vector2(0, yOffset);
-        alternateRectTransOffsetVector2 = new Vector2(0, (maxMenuIndex-2) * yOffset);
+        //rectTransOffsetVector2 = new Vector2(0, yOffset);
+        //alternateRectTransOffsetVector2 = new Vector2(0, (maxMenuIndex-2) * yOffset);
     }
 
     private void Update()
@@ -58,7 +57,7 @@ public class MenuButtonController : MonoBehaviour
                         menuIndex++;
                         if (menuIndex > 1 && menuIndex < maxMenuIndex)
                         {
-                            rectTransform.offsetMax += rectTransOffsetVector2;
+                            rectTransform.offsetMax -= new Vector2(0, -yOffset);
                         }
                     }
                     else
@@ -74,13 +73,13 @@ public class MenuButtonController : MonoBehaviour
                         menuIndex--;
                         if (menuIndex < maxMenuIndex - 1 && menuIndex > 0)
                         {
-                            rectTransform.offsetMax -= rectTransOffsetVector2;
+                            rectTransform.offsetMax -= new Vector2(0, yOffset);
                         }
                     }
                     else
                     {
                         menuIndex = maxMenuIndex;
-                        rectTransform.offsetMax = alternateRectTransOffsetVector2;
+                        rectTransform.offsetMax = new Vector2(0, (maxMenuIndex - 2) * yOffset);
                     }
                 }
                 keyDown = true;
